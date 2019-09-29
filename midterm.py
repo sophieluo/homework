455ca953f34d89dd90995edbcf5629ae7637eaf8
sophieluo <sophie@groobusiness.com>
2019-09-29 15:17:11 -0400

    midterm

---
+++ midterm.py
@@ -0,0 +1,27 @@
+# MIDTERM PLANNING
+# TO BE COMPLETED BEFORE 10/9
+
+# ACCT
+# TODO 1. Lecture notes from statement of cash flows
+# TODO 2. Redo quiz 1 and quiz 2
+# TODO 3. Redo homework 1
+# TODO 4. Create a cheat sheet
+
+
+# STAT
+# TODO 1. Lecture notes module 1-5
+# TODO 2. Lecture notes 5-8
+# TODO 3. Lecture notes 8-12
+# TODO 4. Redo quiz 1 and quiz 2
+
+
+# MEGC
+# TODO 1. Lecture notes
+# TODO 2. Re-do all homework practice problems
+
+
+# CIT
+# TODO 1. Lecture notes review
+# TODO 2. Redo quiz 1-4
+# TODO 3. Redo recitation 1-4
+# TODO 4. Review homeworks
