2cc9333c542a4abbf4d2f088e134a5e4334b36f4
sophieluo <sophie@groobusiness.com>
2019-10-01 23:10:22 -0400

    update todos

--- midterm.py
+++ midterm.py
@@ -4,18 +4,21 @@
 # GENERAL
 # TODO 1: McNulty Leadership
 
+
 # ACCT
 # TODO 1. COMPLETED!! Lecture notes from statement of cash flows
 # TODO 2. Redo quiz 1 and quiz 2
 # TODO 3. Redo homework 1
 # TODO 4. Create a cheat sheet
+# TODO 5: Homework 2
 
 
 # STAT
 # TODO 1. Lecture notes module 1-5
 # TODO 2. Lecture notes 5-8
 # TODO 3. Lecture notes 8-12
-# TODO 4. Redo quiz 1 and quiz 2
+# TODO 4. Redo quiz 1
+# TODO 5: Redo quiz 2
 
 
 # MEGC
