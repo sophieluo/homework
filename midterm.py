52fb8746ba09d049390afd14924205e5944b4b69
sophieluo <sophie@groobusiness.com>
2019-10-15 20:40:56 -0400

    update todos

--- midterm.py
+++ midterm.py
@@ -1,25 +1,7 @@
-# MIDTERM PLANNING
-# TO BE COMPLETED BEFORE 10/9
+# TODOLIST
 
 # GENERAL
-# TODO 1: McNulty Leadership
-# TODO 2: Resume Edit
-
-
-# CIT
-# TODO 1. Lecture notes review
-# TODO 2. Redo quiz 1-4
-# TODO 3. Redo recitation 1-4
-# TODO 4. Review homeworks
-# TODO 5: CIT Cheatsheet
-
-
-# STAT
-# TODO 1. Lecture notes module 1-5
-# TODO 2. Lecture notes 5-8
-# TODO 3. Lecture notes 8-12
-# TODO 4. Redo quiz 1
-# TODO 5: Redo quiz 2
+# TODO 1: McNulty Leadership - self + lydia + teddy
 
 
 # ACCT
@@ -28,7 +10,7 @@
 # TODO 2. Redo quiz 1 and quiz 2
 # TODO 3. Redo homework 1
 # TODO 4. Create a cheat sheet
-# TODO 5: Homework 2
+# TODO 5: Redo homework 2
 
 
 # MEGC
@@ -37,3 +19,5 @@
 # TODO 2. Re-do all homework practice problems
 
 
+# TODO: personal portfolio edit
+# TODO: resume in Chinese & send to Junlu Mao
