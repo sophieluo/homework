ebec364535edf89c519a5a348eedfe7f7ce1157b
sophieluo <sophie@groobusiness.com>
2019-11-27 00:01:51 -0800

    add final todos

---
+++ final.py
@@ -0,0 +1,36 @@
+# TODOLIST (TO BE COMPLETED BEFORE 12/13)
+
+
+# CIT 590
+# TODO: read lecture slides on Java
+# TODO: HW due 12/4!!
+
+
+# MGMT 801
+# TODO: Mini pitch
+# TODO: Prepare for swag day
+# TODO: Final deck
+
+
+# MEGC 612
+# TODO: lecture notes
+
+
+# FNCE 612
+
+
+# WHCP 611
+# TODO: Submit 1-min speech script
+
+
+# ACCT 613
+# TODO: lecture slides
+# TODO: 2018 & 2019 quiz
+
+
+# OTHERS
+# TODO: personal portfolio edit
+# TODO: Sequoia Application
+# TODO: Boston trek planning kickoff
+# TODO: SF trek planning kickoff
+# TODO: GIP coordinator
